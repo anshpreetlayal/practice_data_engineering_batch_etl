1174f9a1704732dfe9481a708db8e300527e7e6a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-03-19 18:41:19 -0400

    added example code

---
+++ Practice_files/EvaluationandCaching.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
+
+object SparkExamples {
+  def main(args: Array[String]): Unit = {
+    // Spark Configuration
+    val conf = new SparkConf().setAppName("SparkExamples").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    // Example RDD
+    val rdd = sc.parallelize(Seq(1, 2, 3, 4, 5))
+
+    // Evaluation Example: Reduce action to sum the RDD
+    val sum = rdd.reduce(_ + _)
+    println(s"Sum of RDD elements: $sum")
+
+    // Iteration Example: Iterative training of a model
+    var model = 0
+    val numIterations = 3
+    val data = sc.parallelize(Seq(1, 2, 3, 4, 5))
+
+    for (i <- 1 to numIterations) {
+      model = data.map(point => updateModel(model, point)).reduce(combineModels)
+    }
+
+  }}
